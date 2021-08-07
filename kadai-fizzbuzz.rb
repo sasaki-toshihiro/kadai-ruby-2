63fbeda197f27d99bfa82532a3d626607e41eb2a
toshihiro.sasaki <toshihiro.sasaki@g.softbank.co.jp>
2021-08-07 06:36:25 +0000

    3rd commit

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,18 +1,17 @@
 
-  def fizzbuzz(num)
-    if num % 15 == 0
-      result = 'FizzBuzz'
-    elsif num % 5 == 0
-      result = 'Buzz'
-    elsif num % 3 == 0
-      result = 'Fizz'
-    else
-      result = num
-    end
-    result
+def fizzbuzz(num)
+  if num % 15 == 0
+    result = 'FizzBuzz'
+  elsif num % 5 == 0
+    result = 'Buzz'
+  elsif num % 3 == 0
+    result = 'Fizz'
+  else
+    result = num
   end
+end
 
-  num_max = 100
-  (1..num_max).each do |num|
-    puts fizzbuzz(num)
-  end
+num_max = 100
+(1..num_max).each do |num|
+  puts fizzbuzz(num)
+end
